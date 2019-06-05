10f8e6eb76f60bea0319eef89d67d5baced36621
nagendraLeela111 <51318403+nagendraLeela111@users.noreply.github.com>
2019-06-05 20:35:06 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,29 @@
 /*
- * top to bottom.c
+ * bottom to top.c
  *
- * Created: 6/4/2019 2:37:40 PM
+ * Created: 6/4/2019 3:27:42 PM
  * Author : Bhargav
- */
-#ifndef F_CPU 
+ */ 
+#ifndef F_CPU
 #define F_CPU 16000000UL
 #endif
+
 
 #include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
-
 int main(void)
 {
-	 
-	int arr[10]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+	int arr[10]={0x10,0x20,0x40,0x80,0x01,0x02,0x04,0x08};
 	DDRD=0xFF;
 	
-    /* Replace with your application code */
-    while (1) 
-    {
+	/* Replace with your application code */
+	while (1)
+	{
 		
-			
-		for(int i=0;i<8;i++)
+		
+		for(int i=7;i>=0;i--)
 		{
 			
 			PORTD=arr[i];
@@ -32,6 +31,8 @@ int main(void)
 			
 		}
 		
-    }
+	}
 }
+
+    
 

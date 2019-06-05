17b37da6b44560328859b32fb71687ab60bb0ecc
nagendraLeela111 <51318403+nagendraLeela111@users.noreply.github.com>
2019-06-05 20:34:22 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,25 +1,37 @@
 /*
- * blinking.c
+ * top to bottom.c
  *
- * Created: 6/4/2019 12:36:50 PM
+ * Created: 6/4/2019 2:37:40 PM
  * Author : Bhargav
- */ 
-#ifndef F_CPU
+ */
+#ifndef F_CPU 
 #define F_CPU 16000000UL
 #endif
 
+#include <stdio.h>
 #include <avr/io.h>
-#include<util/delay.h>
+#include <util/delay.h>
+
 
 int main(void)
 {
-    DDRD = 0xFF;
+	 
+	int arr[10]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+	DDRD=0xFF;
+	
+    /* Replace with your application code */
     while (1) 
     {
-		PORTD=0xFF;
-		_delay_ms(220);
-		PORTD=0x00;
-		_delay_ms(220);
+		
+			
+		for(int i=0;i<8;i++)
+		{
+			
+			PORTD=arr[i];
+			_delay_ms(220);
+			
+		}
+		
     }
 }
 
